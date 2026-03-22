23971c1194a979611a697e7bea8114ed148676ad
Anthony Saleeb <anthonysaleeb@gmail.com>
2026-03-22 22:11:22 +1100

    tt

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oneanother.app',
+  appName: '1Another',
+  webDir: 'public'
+};
+
+export default config;

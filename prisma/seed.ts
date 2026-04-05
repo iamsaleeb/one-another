8934800c51b53b864545c8f1a221667a6f87c030
Anthony Saleeb <anthonysaleeb@gmail.com>
2026-04-05 23:22:51 +1000

    added package fixes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,12 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcryptjs";
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) throw new Error("DATABASE_URL environment variable is not set");
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 /** Return a Date that is `days` from now, at the given time-of-day (HH:MM). */
 function future(days: number, time = "10:00"): Date {
